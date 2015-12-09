7c4bea79f501d7f819d615fd8f7e442f2c01fe98
pavan8632 <pavan8632@gmail.com>
2015-12-09 15:17:11 -0800

    Finishing requirements

--- global.r
+++ global.r
@@ -1,7 +1,7 @@
 
 #check for and/or install dependencies
 #NEED TO ADD CHECK FOR NFACTOR
-need<-c("RCurl","ggplot2","gridExtra",'png','mice','missMDA','homals','memoise','png','grid','shinyjs','data.table')
+need<-c("RCurl","ggplot2","gridExtra",'png','mice','missMDA','homals','memoise','png','grid','shinyjs','data.table','shinythemes')
 for(i in 1:length(need)){
   if(require(need[i], character.only = TRUE)==FALSE){ install.packages(need[i]);library(need[i], character.only = TRUE)} else { library(need[i],character.only = TRUE)}
 }
